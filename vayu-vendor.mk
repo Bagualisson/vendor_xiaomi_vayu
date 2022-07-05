5a6dd7d45270758c6d221e03424bbf59195c0c00
orges <me@orgesified.ml>
2022-07-05 22:59:07 +0200

    vayu: update adreno blobs to LA.UM.9.14.r1-19300-LAHAINA.QSSI13.0
    Signed-off-by: orges <me@orgesified.ml>

--- vayu-vendor.mk
+++ vayu-vendor.mk
@@ -1134,7 +1134,6 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.msmnile.api30 \
     PowerOffAlarm \
     HotwordEnrollmentOKGoogleHEXAGON \
     HotwordEnrollmentXGoogleHEXAGON \
